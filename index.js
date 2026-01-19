3f98d8b6fba121683c4dfcf23543cb3a8e808e69
Silas17 <silascs120@gmail.com>
2026-01-19 08:57:34 -0300

    Estudando Function

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-torrar("pao de forma",undefined, 10.90);
+//torrar("pao de forma",undefined, 10.90);
 
 
 function torrar(pao, nome = 'Silas', valor) {
     console.log("torrada feita com" + pao);
     console.log("ela e um pedido de " + nome);
     console.log("valor total: " + valor);
+}
+
+porta("fechada");
+
+function porta(status){
+    if(status === "aberta"){
+        status = "fechada";
+        console.log("porta " + status);
+    } else {
+        status = "aberta";
+        console.log("porta " + status);
+    }
 }
